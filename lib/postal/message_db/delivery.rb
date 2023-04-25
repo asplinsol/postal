c3a57f9abb23f17075e0c60d746b5d33fac6760b
John Asplin <john@asplinsolutions.co.uk>
2023-04-25 13:29:59 +0100

    "Fix FrozenError by using encode method instead of force_encoding"

--- lib/postal/message_db/delivery.rb
+++ lib/postal/message_db/delivery.rb
@@ -50,7 +50,7 @@ module Postal
           :message => @message.webhook_hash,
           :status => self.status,
           :details => self.details,
-          :output => self.output.to_s.force_encoding('UTF-8').scrub,
+          :output => self.output.to_s.encode('UTF-8', invalid: :replace, undef: :replace, replace: '').scrub
           :sent_with_ssl => self.sent_with_ssl,
           :timestamp => @attributes['timestamp'],
           :time => self.time
